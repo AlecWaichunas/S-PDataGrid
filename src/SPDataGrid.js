632a2d005a94eb5dfc1483516dd04e4d2a102f2b
Alec Waichunas <awaichunas@gmail.com>
2020-07-16 13:13:29 -0500

    Slider functionality added

--- src/SPDataGrid.js
+++ src/SPDataGrid.js
@@ -1,12 +1,32 @@
 import React from 'react';
 import data from './SPData.json';
+import { Range } from 'rc-slider';
+import 'rc-slider/assets/index.css';
 
 class SPDataGrid extends React.Component {
+
+    constructor(props){
+        super(props);
+        //set state for data and slider
+        this.state = {
+            min: 0,
+            max: 0,
+            value: [0, 0],
+            loaded: false
+        };
+    }
 
     //reverse the data to put in ascending
     componentDidMount(){
         data.reverse();
-        this.setState({});
+        var min = data[0].year;
+        var max = data[data.length - 1].year;
+        this.setState({
+            min: min,
+            max: max,
+            value: [min, max],
+            loaded: true
+        });
     }
 
     //render data grid
@@ -31,24 +51,36 @@ class SPDataGrid extends React.Component {
         });
     }
 
+    //update slider values
+    onSliderChange = value => {
+        this.setState({ value });
+    }
+
     //draw grid using HTML DOM
     render(){
-        return (
-            <>
-                <table>
-                    <thead>
-                        <tr>
-                            <th>Year</th>
-                            <th>Total Return</th>
-                            <th>Culmative Returns</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {this.updateDataGrid(2000, 2005)}
-                    </tbody>
-                </table>
-            </>
-        );
+        const {min, max, value, loaded} = this.state;
+        if(loaded)
+            return (
+                <>
+                    <Range allowCross={false} 
+                            min={min} max={max} value={value}
+                            onChange={this.onSliderChange}/>
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>Year</th>
+                                <th>Total Return</th>
+                                <th>Culmative Returns</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {this.updateDataGrid(value[0], value[1])}
+                        </tbody>
+                    </table>
+                </>
+            );
+        else
+            return (<h3>Loading Data...</h3>)
     }
 
 }
